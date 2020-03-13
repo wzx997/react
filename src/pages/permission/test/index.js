193de6f41536b2f436e0ce269a2c496184c42551
王志祥 <1181659294@qq.com>
2020-03-13 18:06:26 +0800

    测试邮箱修改成功没有

--- src/pages/permission/test/index.js
+++ src/pages/permission/test/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import {
-    Modal,
-    Row,
-    Col,
-    Button,
-    message,
+    Modal, Row, Col, Button, message,
     Card, Tooltip, notification, Table, Alert, Spin
 } from 'antd';
 
